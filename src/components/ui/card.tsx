ced3e902c8b667415a5cac3d27289d3bb85d2549
Glormy <tomjodoinux@gmail.com>
2026-03-30 12:01:45 -0700

    Updated Colors, Buttons, and Cards
    Updated brand green and greys for light and dark mode. Updated Primary and Secondary buttons. Updated cards.

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -7,7 +7,7 @@ function Card({ className, ...props }: React.ComponentProps<"div">) {
     <div
       data-slot="card"
       className={cn(
-        "bg-card text-card-foreground flex flex-col gap-6 rounded-xl border py-6 shadow-sm",
+        "bg-card text-card-foreground flex h-[206px] w-[256px] shrink-0 flex-col gap-4 rounded-xl py-4",
         className
       )}
       {...props}
